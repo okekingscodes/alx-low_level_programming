da786898cc0413b8012f4b921ee144164193bac8
Odim Kingsley <okekings@gmail.com>
2022-07-05 01:09:47 -0700

    Printing alphabet 10x in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -> prints the lowercase alphabets
+ */
+
+void print_alphabet_x10(void);
+{
+	int j;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+		_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
