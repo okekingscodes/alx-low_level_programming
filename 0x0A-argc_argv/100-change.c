38b8ce630323e621af57894690627ebdbc465282
Odim Kingsley <okekings@gmail.com>
2022-07-22 11:17:22 -0700

    A program that prints the minimum number of coins to make change for an amount

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
-* main -> This is a function that add two numbers using argc and argv
-* @argc: argc is a parameter
-* @argv: an array of a command line listed
-* Return: result
-*/
+ * main - prints the minimum number of coins to make change for an amount.
+ * of money.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
 int main(int argc, char *argv[])
 {
-	int num, j, result = 0;
-	int coins[] = {25, 10, 5, 2, 1};
+	int cents, ncoins = 0;
 
-	if (argc != 2)
+	if (argc == 1 || argc > 2)
 	{
-		printf("%s\n", "Error");
+		printf("Error\n");
 		return (1);
 	}
-	num = atoi(argv[1]);
-	if (num < 0)
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
 	{
-		printf("0\n");
-		return (0);
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
 	}
-	for (j = 0; j < 5 && num >= 0; j++)
-	{
-		while (num >= coins[j])
-		{
-			num -= coins[j];
-			result++;
-		}
-	}
-	printf("%d\n", rsult);
+	printf("%d\n", ncoins);
 	return (0);
 }
