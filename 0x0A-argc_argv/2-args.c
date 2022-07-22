a5d2f256b9404c2c29e5b5938697501613ba5e52
Odim Kingsley <okekings@gmail.com>
2022-07-22 10:49:27 -0700

    A function that prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main -> This is a function that print its file name
+* main -> This is a function that print all arguments it receives
 * @argc: argc is a parameter
 * @argv: an array of a command line listed
 * Return: 0 for success
@@ -11,8 +11,6 @@ int main(int argc, char *argv[])
 	int i;
 
 	for (i = 0; i < argc; i++)
-	{
-	printf("%s\n", argv[i]);
-	}
+		printf("%s\n", argv[i]);
 	return (0);
 }
