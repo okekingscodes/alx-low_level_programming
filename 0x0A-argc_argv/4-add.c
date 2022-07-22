8618cc163bebe4fac9a85a3f6139d16201c8a0ea
Odim Kingsley <okekings@gmail.com>
2022-07-22 10:36:15 -0700

    A program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main -> This is a function that add two numbers using argc and argv
+* @argc: argc is a parameter
+* @argv: an array of a command line listed
+* Return: result
+*/
+int main(int argc, char *argv[])
+{
+	int result = 0, num, i, j, k;
+
+	for (i = 1; i < argc; i++
+	{
+		for (j = 0; argv[i][j] != '\0'; j++
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+		}
+	}
+	for (k = 1; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		result += num;
+	}
+	printf("%d\n", result);
+	return (0);
+}
