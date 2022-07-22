058b9f62e720e02225b63ec820f3071a93199c41
Odim Kingsley <okekings@gmail.com>
2022-07-22 11:11:05 -0700

    A program that add two positive numbers' :

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++
 	{
-		for (j = 0; argv[i][j] != '\0'; j++
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > '9' || argv[i][j] < '0')
 			{
