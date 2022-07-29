f848742c0e54104a6f788ea7aa128520efe17708
Odim Kingsley <okekings@gmail.com>
2022-07-29 05:51:39 -0700

    0x0C. C - More malloc, free tasks

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower -> this program checks if the character is lowercase
+ * @c: a character argument
+ * Return: returns 1 and 0 depending on condition
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
